6ac7b62fa35e51468cfd7893f1a94aeb0895d0b2
itsPeetah <pietro.g.moroni@gmail.com>
2022-05-12 21:00:52 +0200

    Added routes and Static main nav push method

--- dima/lib/util/navigation/main_routes.dart
+++ dima/lib/util/navigation/main_routes.dart
@@ -1,9 +1,17 @@
+import 'package:dima/pages/signin.dart';
 import 'package:flutter/material.dart';
 import 'package:dima/components/navigation/main_navigation.dart';
 import 'package:dima/pages/404.dart';
 
 class MainNavigator {
   static final mainNavigatorKey = GlobalKey<NavigatorState>();
+
+  static void push(String route) {
+    if (route[0] != "/") route = "/" + route;
+    final rs = RouteSettings(name: route);
+    final r = MainNavigatorRouter.generateRoute(rs);
+    mainNavigatorKey.currentState!.push(r!);
+  }
 }
 
 class MainNavigationRoutes {
@@ -12,6 +20,7 @@ class MainNavigationRoutes {
   static const String payment = "/payment";
   static const String checkoutResult = "/checkoutResult";
   static const String signin = "/signin";
+  static const String register = "/register";
 }
 
 // Generate routes for the main navigation
@@ -21,6 +30,8 @@ class MainNavigatorRouter {
       /* Put here the cases for checkout, payment... */
       case MainNavigationRoutes.root:
         return MaterialPageRoute(builder: (_) => const MainNavigation());
+      case MainNavigationRoutes.signin:
+        return MaterialPageRoute(builder: (_) => SignInPage());
       default:
         return MaterialPageRoute(
             builder: (_) => const Scaffold(body: PageNotFound()));
