226403549bbbf9830980c350e7fcbc5ab8897667
itsPeetah <pietro.g.moroni@gmail.com>
2022-05-14 10:23:59 +0200

    Create catalogue_list.dart

---
+++ dima/lib/widgets/product/catalogue_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductCatalogueList extends StatelessWidget {
+  const ProductCatalogueList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("List here...");
+  }
+
+  // List<Widget> _buildProductRows(){
+  //   List<Widget> rows = [];
+
+  // }
+}
