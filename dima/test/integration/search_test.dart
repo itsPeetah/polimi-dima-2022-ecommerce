f0a7e27e000680d2d03c5c82959ed1cd652d33d0
itsPeetah <pietro.g.moroni@gmail.com>
2022-07-14 00:24:43 +0200

    Test search

---
+++ dima/test/integration/search_test.dart
@@ -0,0 +1,46 @@
+import 'package:dima/main.dart';
+import 'package:dima/pages/authentication/register.dart';
+import 'package:dima/pages/authentication/signin.dart';
+import 'package:dima/pages/search_products.dart';
+import 'package:dima/util/database/database.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  testWidgets('Check that the Search page works correctly',
+      (WidgetTester tester) async {
+    DatabaseManager.updateProductTester();
+    // await tester.pumpWidget(
+    //   MaterialApp(home: Center(child: ProductSearchPage())),
+    // );
+    MyApp _myApp = const MyApp();
+    await tester.runAsync(() async {
+      await tester.pumpWidget(ChangeNotifierProvider(
+        create: (context) => ApplicationState(initializer: () {
+          return;
+        }),
+        builder: (context, _) => _myApp,
+      ));
+    });
+    // await tester.pumpAndSettle();
+    await tester.tap(find.byIcon(Icons.search_rounded));
+    await tester.pump(Duration(seconds: 1));
+
+    for (final element
+        in find.byType(TextField, skipOffstage: false).evaluate()) {
+      TextField widget = element.widget as TextField;
+      widget.controller!.text = 'asasasasa';
+      // await tester.tapAt(tester.getCenter(find.byWidget(element.widget)));
+    }
+    await tester.pump(Duration(seconds: 1));
+    expect(find.textContaining("Oops"), findsOneWidget);
+    // expect(find.textContaining('Sign Up', skipOffstage: false), findsOneWidget);
+    // expect(
+    //     find.textContaining('Full Name', skipOffstage: false), findsOneWidget);
+    // expect(find.textContaining('Email', skipOffstage: false), findsOneWidget);
+    // expect(
+    //     find.textContaining('Password', skipOffstage: false), findsOneWidget);
+    // expect(find.textContaining('Cancel', skipOffstage: false), findsOneWidget);
+  });
+}
